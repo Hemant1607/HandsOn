297f99648ab4a11ab9960dcd148849439058ca50
Hemant1607 <hemant1607.hk@gmail.com>
2021-09-21 19:42:50 +0530

    21/09/2021 HandsOn

---
+++ DictionaryHandsOn/DictionaryHandsOn/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryHandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> d = new Dictionary<int, string>();
+            d.Add(1, "Player 5");
+            d.Add(2, "Player 1");
+            d.Add(3, "Player 3");
+            d.Add(4, "Player 2");
+            d.Add(5, "Player 4");
+
+            Console.WriteLine("Total number of players in race: {0}", d.Count());
+            d[1] = "Player 4";
+            d[5] = "Player 5";
+            foreach (KeyValuePair<int,string> i in d)
+            {
+                Console.WriteLine("Player at position {0} is {1}", i.Key, i.Value);
+            }
+            d.Clear();
+            Console.Read();
+        }
+    }
+}
